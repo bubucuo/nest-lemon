8cdd089a0d25e87befacf5905de2b86f32e30d8e
bubucuo <276185876@qq.com>
2024-07-03 00:00:10 +0800

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { UserModule } from './user/user.module';
+import { User2Module } from './user2/user2.module';
+
 import { AuthModule } from './auth/auth.module';
-// import { MongooseModule } from '@nestjs/mongoose';
+import { MongooseModule } from '@nestjs/mongoose';
 
 // 装饰器
 @Module({
@@ -22,7 +24,10 @@ import { AuthModule } from './auth/auth.module';
     AuthModule,
   ],
 
-  // imports: [MongooseModule.forRoot('mongodb://localhost/nest')],
+  // imports: [
+  //   MongooseModule.forRoot('mongodb://localhost:27017/template-server'),
+  //   User2Module,
+  // ],
 })
 export class AppModule {
   // constructor(private dataSource: DataSource) {}
